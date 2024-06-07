3314e4397a6fb79a5236876470a0d710687e51e8
jordanoboyle <jmcmcode@gmail.com>
2024-06-07 10:43:45 -0600

    sessions controller generate (login)

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
 
   # Users Controller Routes
   post "/users",       controller: "users", action: "create"
+
+  # Sessions Controller Route
+  post "/sessions",    controller: "sessions", action: "create"
 end
